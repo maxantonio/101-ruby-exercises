538ebfd82573f8964d40d86b67ba8a8200e4a54d
dragon <maxantonio@my.dragon>
2013-07-02 20:56:24 -0400

    001

---
+++ 0-10/001.rb
@@ -0,0 +1,10 @@
+# 001 The most basic excercise
+
+puts "Hello what's you name"
+name = gets.chomp
+puts "Nice to meet you #{name}"
+puts "what is your favorite color"
+color = gets.chomp
+puts "That's Interesting"
+
+
