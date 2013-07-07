0783cc57be4c3ba1739977145eeb5245dc1faddd
dragon <maxantonio@my.dragon>
2013-07-06 22:09:52 -0400

    002

---
+++ 0-10/002.rb
@@ -0,0 +1,8 @@
+# 002 Ternary operartor
+
+puts "How old are you?"
+age = gets.chomp.to_i
+puts age > 18 ? "you can play in rubycasino" : "law prohibits anyone under the age of 18 to enter the casino"
+
+
+
